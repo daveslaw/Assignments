11607c6fd34fa25ed0c26e2425e300946a0f5edd
Dave Lawrence <daveslaw@gmail.com>
2021-05-12 18:59:43 +0300

    Milestone 6

--- Fibonacci/index.js
+++ Fibonacci/index.js
@@ -1,16 +1,16 @@
 // Fibonacci array generator
-let fibArray = [0, 1,]; 
-console.log("This is original array: " + fibArray);
+let fibArray = [0, 1]; 
 for (let i = 1; i <= 50; i++) {
   let fibArrayItem = fibArray[i-1] + fibArray[i];
   fibArray.push(fibArrayItem);
 }
 
+
 // Input
 const fibonacciInput = document.getElementById("fibonacciInput");
 const inputButton = document.getElementById("inputButton");
-console.log(fibonacciInput);
-console.log(inputButton);
+//console.log(fibonacciInput);
+//console.log(inputButton);
 
 //Button functionality
 const onInput = (event) => {
@@ -21,13 +21,108 @@ const onInput = (event) => {
     }
 };
 
-//Function for submitting Output 
-const submitInput = () => {
-  document.getElementById("fibonacciOutput").innerHTML = fibArray[fibonacciInput.value];
+const spinner = document.getElementById("spinner");
+const errorMessage = document.createElement("div");
+errorMessage.classList.add("error-message");
+
+
+
+let fibCalc = document.getElementById("fib-calc")
+
+//Function for submitting Input 
+const submitInput = () => {   
+  
+  if (fibonacciInput.value > 50) {
+    document.getElementById("fibonacciOutput").innerHTML = ""
+    fibonacciInput.style.color = "red";
+    fibonacciInput.style.border = "red 1px solid";
+    errorMessage.textContent = "Can't be over 50";
+    fibCalc.appendChild(errorMessage);
+    
+  }
+  else if (fibonacciInput.value <= 50 && fibonacciInput.value > -1 ) {
+    spinner.removeAttribute('hidden');   
+   
+    const url = "http://localhost:5050/fibonacci/" + fibonacciInput.value;
+    console.log(url);  
+    fetch(url)
+    .then((response) => {
+      //console.log(response); 
+      if (response.ok) {
+        response.json()
+        .then((data) => {
+          //console.log(data);
+          spinner.setAttribute('hidden', '');      
+          document.getElementById("fibonacciOutput").innerHTML = data.result;
+        });
+      } else {
+        response.text()
+        .then((error) => {
+          spinner.setAttribute('hidden', '');  
+          document.getElementById("fibonacciOutput").innerHTML = error;
+        });
+      }
+      
+    })
+    
+
+  }
+  else if (fibonacciInput.value < 0) {
+    fibonacciInput.style.color = "red";
+    fibonacciInput.style.border = "red 1px solid";
+    errorMessage.textContent = "Can't be less than 1";
+    fibCalc.appendChild(errorMessage);
+  }  
 };
+
+const fibLog = () => {
+  
+  urlFibResults = "http://localhost:5050/getFibonacciResults";
+
+  fetch(urlFibResults)  
+  .then((response) => response.json())
+  .then((data) => {
+    //console.log(data);
+    //console.log(data.results);
+    //console.log(data.results[1]);
+    //let dateArray = [];
+    data.results.forEach((fibResult) => {
+    const inputNumber = (fibResult["number"]);
+    //console.log("Input number: " + inputNumber)
+    const outputNumber = (fibResult["result"]);
+    //console.log("Output number: " + outputNumber)
+    
+    const rawDate = (fibResult["createdDate"]);
+    
+    let formattedDate = new Date(rawDate);
+    //dateArray.push(rawDate);
+
+    console.log(`The fibonacci of ${inputNumber} is ${outputNumber}. Calculated at: ${formattedDate}`);
+       
+    
+    
+    //console.log("Date: " + formattedDate );
+
+
+    })
+    //console.log(dateArray);
+    // function descendDate() {
+    //   dateArray.sort(function(a, b){return b-a});   
+    // }
+
+    
+    // function ascendDate() {
+    //   dateArray.sort(function(a, b){return a-b});   
+    // }
+    
+  })
+       
+  
+
+}   
+
+fibLog();
 
 
 fibonacciInput.addEventListener("input", onInput);
-inputButton.addEventListener("click", submitInput);
-
-
+inputButton.addEventListener("click", submitInput);  
